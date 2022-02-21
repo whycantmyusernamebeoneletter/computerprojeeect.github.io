4400743ef7a68ad3467c9aee5a7d48db21bd849e
whycantmyusernamebeoneletter <100155024+whycantmyusernamebeoneletter@users.noreply.github.com>
2022-02-21 21:18:20 +0300

    ....

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const hamburger=document.querySelector('.hamburger');
 hamburger.addEventListener('click' , function () {
     this.classlist.toggle('is-active');
 });
+<a href=" html.html " onclick="window.open('example.html', '_self');">
+
+computerprojeeect.github.io
+
+</a>
--- sara and rasha/main.js
+++
@@ -1,5 +0,0 @@
-const hamburger=document.querySelector('.hamburger');
-
-hamburger.addEventListener('click' , function () {
-    this.classlist.toggle('is-active');
-});
